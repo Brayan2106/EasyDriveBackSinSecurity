171a569e9131cf47540ec767b20f8fa1367be4a5
Brayan2106 <brayan.flores.210602@gmail.com>
2024-11-18 18:13:15 -0500

    a

---
+++ src/main/java/pe/edu/upc/easydrive/EasyDriveBackSinSecurityApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.easydrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDriveBackSinSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyDriveBackSinSecurityApplication.class, args);
+    }
+
+}
